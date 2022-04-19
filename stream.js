4e4d71be702ee9a3ae58d84b8a6118668a8e68b3
Md Abu Ryan <67921639+mdaburyan@users.noreply.github.com>
2022-04-20 00:39:00 +0600

    Delete stream.js

--- stream.js
+++
@@ -1,6 +0,0 @@
-const fs=require('fs');
-const readSteam=fs.createReadStream('./docs/blog3.txt')
-readSteam.on('data',(chunk)=>{
-    console.log('---New Chunk---');
-    console.log(chunk.toString())
-})
